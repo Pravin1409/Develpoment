30b6eda9d48af4520ddad24d8a490812258a67cc
Pravin1409 <93812415+Pravin1409@users.noreply.github.com>
2022-01-02 12:15:28 +0530

    Add files via upload
    add

---
+++ 10.c
@@ -0,0 +1,18 @@
+  #include<stdio.h> 
+  void Display()
+  {
+    int i=0;
+    //    1    2   3
+    for(i=1;i<=5;i++)
+    {
+        printf("marvellous\n "); //4
+      
+    }
+  }
+
+
+  int main()
+  {
+      Display(); // function call
+      return 0;
+  }
